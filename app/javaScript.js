0fb81f881772ba679023dc3c9006ff460887afc5
bpillet <bruno.pillet@arturia.com>
2024-06-21 11:31:50 +0200

    adding PAT support to move the paddles

--- app/javaScript.js
+++ app/javaScript.js
@@ -1,3 +1,72 @@
+function connect() {
+  navigator.requestMIDIAccess()
+  .then(
+    (midi) => midiReady(midi),
+    (err) => console.log('Something went wrong', err));
+}
+connect()
+
+function midiReady(midi) {
+  // Also react to device changes.
+  console.log("Midi granted")
+  midi.addEventListener('statechange', (event) => initDevices(event.target));
+  initDevices(midi); // see the next section!
+}
+
+function initDevices(midi) {
+  // Reset.
+  midiIn = [];
+  midiOut = [];
+  
+  // MIDI devices that send you data.
+  const inputs = midi.inputs.values();
+  for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
+    midiIn.push(input.value);
+  }
+  
+  // MIDI devices that you send data to.
+  const outputs = midi.outputs.values();
+  for (let output = outputs.next(); output && !output.done; output = outputs.next()) {
+    midiOut.push(output.value);
+  }
+  
+  // displayDevices();
+  startListening();
+}
+
+
+// Start listening to MIDI messages.
+function startListening() {
+  for (const input of midiIn) {
+    input.addEventListener('midimessage', midiMessageReceived);
+  }
+}
+
+function midiMessageReceived(event) {
+  // MIDI commands we care about. See
+  // http://webaudio.github.io/web-midi-api/#a-simple-monophonic-sine-wave-midi-synthesizer.
+  const NOTE_ON = 9;
+  const NOTE_OFF = 8;
+
+  console.log(event)
+  
+  // You can use the timestamp to figure out the duration of each note.
+  const timestamp = Date.now();
+  if (event.data[0] == 160){ //polyAT
+    console.log(event.data)
+    if (event.data[1] == P1_KEY_ID){ //player1
+      p1.paddle.setY(event.data[2]/127*CANVAS_HEIGHT)
+    }else if (event.data[1] == P2_KEY_ID){ //player2
+      p2.paddle.setY(event.data[2]/127*CANVAS_HEIGHT)
+    }
+  }
+  
+  
+}
+
+
+const P1_KEY_ID = 96
+const P2_KEY_ID = 84
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d"); 
